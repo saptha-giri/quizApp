76444f44e1784122240255564f62fbe3533ed6a6
intentbi3@gmail.com <intentbi3@gmail.com>
2021-06-14 22:27:03 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const onPressLearnMore = () => {
+    console.log("Button Pressed!!!")
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={onPressLearnMore}
+        style={styles.btn}
+      >
+        <Text style={styles.btnText}>Click Me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +28,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
+    borderWidth: 1,
+    borderColor:"red"
   },
+  btn: {
+    width:200,
+    borderWidth: 1,
+    borderColor: "blue",
+    display: "flex",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: "blue"
+  },
+  btnText: {
+    color: "#fff",
+    padding: 5,
+    fontSize: 20,
+    
+  }
 });
